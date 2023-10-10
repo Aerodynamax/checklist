afba063d7f1c7048cae41d57e23cfac906aa4783
Max <maxmillion.bunker@gmail.com>
2023-10-10 17:07:44 +1100

    added back button

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+if (!link) {
+  link = document.createElement("link");
+  link.rel = "icon";
+  document.head.appendChild(link);
+}
+
+link.href = import.meta.env.DEV
+  ? "https://img.icons8.com/windows/512/FF0000/dev.png"
+  : "https://img.icons8.com/cotton/512/checklist--v1.png";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
